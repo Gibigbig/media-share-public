5143821e2d48435d53af453a54a12c6d444e7e16
gibigbig <gibigbig@hotmail.com>
2024-01-15 01:46:16 -0400

    config template

---
+++ src/lib/config.js
@@ -0,0 +1,12 @@
+let config = {};
+
+//you probably dont even need these because tmi.js can read public chats without auth
+
+config = {
+  "password": "twitchpasswordhere",
+  "channel": "gibigbig",
+  "username": "Shoob",
+  "twitchToken": "twitchtokenhere",
+}
+
+export default config;
